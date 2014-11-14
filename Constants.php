343fe98ab5b8c1a9352ab95ebfee9e77f0185132
dmills <dmills@iastate.edu>
2014-11-13 22:10:20 -0600

    first commit

---
+++ Constants.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(E_ALL);
+date_default_timezone_set("America/Chicago");
+
+/* MYSQL DATABASE SERVER */
+define('DB_SERVER', '');
+
+/* MYSQL DATABASE USERNAME */
+define('DB_USER', '');
+
+/* MYSQL DATABASE PASSWORD */
+define('DB_PASS', '');
+
+/* MYSQL DATABASE NAME */
+define('DB_NAME', '');
+?>
---
+++ Result.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Used as a wrapper for prepared statements after they're executed.
+ */
+class Result {
+
+    private $bound_variables;
+    private $results;
+    private $statement;
+    
+    /** 
+     * Constructor - 
+     * Creates a result object from a prepared statement
+     */
+	public function __construct($statement) {
+        $this->statement = $statement;
+		$this->bound_variables = array();
+        $this->results = array();
+        
+        $meta = $this->statement->result_metadata();
+        if ($meta) {
+            while ($column = $meta->fetch_field()) {
+                $this->bound_variables[$column->name] =& $this->results[$column->name];
+            }
+            call_user_func_array(array($this->statement, 'bind_result'), $this->bound_variables); 
+            $meta->close();
+        }
+	}
+    
+    /**
+     * Destructor - 
+     * called as soon as there are no references left to this object
+     * closes the mysqli statement
+     */
+    public function __destruct() {
+        $this->statement->close();
+    }
+    
+    /**
+     * Returns the original statement object
+     */
+    public function get_statement() {
+        return $this->statement;
+    }
+    
+    /**
+     * Fetch result while it can, returns false when finished
+     */
+    public function fetch() {
+        return $this->statement->fetch() ? $this->results : false;
+    }
+    
+    /**
+     * count the number of rows in the result
+     */
+    public function rows() {
+        return $this->statement->num_rows();
+    }
+    
+    /**
+     * Fetch a result row as an associative array, returns false when finished
+     */
+     public function fetch_array() {
+        $results = $this->fetch();
+        if ($results) {
+            $row = array();
+            foreach($results as $key=>$value) {
+                $row[$key] = $value;
+            }
+            return $row;
+        }
+        return $false;
+     }
+    
+    /**
+     * retrieve the last id that was inserted 
+     */
+    public function last_id() {
+        return $this->statement->insert_id;
+    }
+}
